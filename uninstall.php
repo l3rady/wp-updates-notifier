7f2e1b743ae752009b7025b0dc6d1689684853b3
timatron <timatron@users.noreply.github.com>
2019-12-11 15:23:56 +0000

    Bringing up to PHP WP coding standards
    * Downgraded a few codesniffer rules, but generally modified the code to resolve all issues

--- uninstall.php
+++ uninstall.php
@@ -1,24 +1,38 @@
 <?php
-/*  Copyright 2015  Scott Cariss  (email : scott@l3rady.com)
+/**
+ * Uninstall function for plugin.
+ *
+ * This file is read by WordPress to generate the plugin information in the
+ * admin panel.
+ *
+ * @link    https://github.com/l3rady/wp-updates-notifier
+ * @since   1.5.0
+ * @package SC_WP_Updates_Notifier
+ */
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+/**
+Copyright 2015 Scott Cariss (email:scott@l3rady.com)
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-if ( !defined( "WP_UNINSTALL_PLUGIN" ) ) {
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+@package
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 
-delete_option( "sc_wpun_settings" );
-delete_option( "sc_wpun_settings_ver" );
+delete_option( 'sc_wpun_settings' );
+delete_option( 'sc_wpun_settings_ver' );
