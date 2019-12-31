c2024e956911f97b06d90d2e41a735008caaf1e0
timatron@gmail.com <timatron@users.noreply.github.com>
2019-12-31 12:00:26 +0100

    1.5.0 -> 1.5.1

--- class-sc-wp-updates-notifier.php
+++ class-sc-wp-updates-notifier.php
@@ -15,7 +15,7 @@
  * Plugin URI: https://github.com/l3rady/wp-updates-notifier
  * Description: Sends email to notify you if there are any updates for your WordPress site. Can notify about core, plugin and theme updates.
  * Contributors: l3rady, eherman24, alleyinteractive
- * Version: 1.5.0
+ * Version: 1.5.1
  * Author: Scott Cariss
  * Author URI: http://l3rady.com/
  * Text Domain: wp-updates-notifier
